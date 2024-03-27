66962879ccae884738adfc364bf9a6be6b0fd566
s-egge <ssteele836@gmail.com>
2024-03-27 11:31:09 -0700

    Nav drawer added
    - Replaced bottom navbar with a nav-drawer
    - Removed some commented out code and code used for testing

--- app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/BookDetailFragment.kt
+++ app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/BookDetailFragment.kt
@@ -258,7 +258,7 @@ class BookDetailFragment : Fragment() {
 
             // navigate back to the library (clear backwards nav first so you can't go back
             // to the book details page of the deleted book)
-            findNavController().popBackStack(R.id.library_fragment, true)
+            findNavController().popBackStack(R.id.library, true)
 
             dialog.dismiss()
         }
--- app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/MainActivity.kt
+++ app/src/main/java/edu/oregonstate/cs492/bookkeeper/ui/MainActivity.kt
@@ -2,8 +2,11 @@ package edu.oregonstate.cs492.bookkeeper.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.updatePadding
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,33 +15,37 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import edu.oregonstate.cs492.bookkeeper.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfig: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val navHostFragment = supportFragmentManager.findFragmentById(
             R.id.nav_host_fragment
         ) as NavHostFragment
-        val navController = navHostFragment.navController
-        appBarConfig = AppBarConfiguration(navController.graph)
+        navController = navHostFragment.navController
 
-        val appBar: MaterialToolbar = findViewById(R.id.top_app_bar)
-        setSupportActionBar(appBar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        appBarConfig = AppBarConfiguration(setOf(
+            R.id.library,
+            R.id.browse_books
+        ), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfig)
 
-        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
-        bottomNav.setupWithNavController(navController)
-        bottomNav.setOnApplyWindowInsetsListener { view, insets ->
-            view.updatePadding(bottom = 0)
-            insets
-        }
+        findViewById<NavigationView>(R.id.nav_view)?.setupWithNavController(navController)
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
